cfcb108a928c47720edd471cb8658337b0945ee1
Marko Nikolajevic <mrknikolajevic@gmail.com>
2019-05-10 18:19:33 +0200

    Add App.js

---
+++ src/assets/js/App.js
@@ -0,0 +1,23 @@
+import React from 'react'
+import '../style/App.css'
+import Header from './components/Header'
+import AboutMe from './components/AboutMe'
+import Portfolio from './components/Portfolio'
+import Form from './components/Form'
+import Footer from './components/Footer'
+
+
+function App() {
+  return (
+    <div>
+      <Header />
+      <AboutMe />
+      <Portfolio />
+      <Form />
+      <Footer />
+    </div>
+  )
+}
+
+
+export default App

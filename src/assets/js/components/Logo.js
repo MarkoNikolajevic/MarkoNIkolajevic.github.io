68d2543df5ecb33c4c40f2d002cd51f3b7259919
Marko Nikolajevic <mrknikolajevic@gmail.com>
2019-05-12 15:46:00 +0200

    Correct typo

--- src/assets/js/components/Logo.js
+++ src/assets/js/components/Logo.js
@@ -6,7 +6,7 @@ function Logo() {
   return (
     <div className='brand-logo'>
       <picture>
-        <source media='min-width: 768px' srcSet={logoLarge} />
+        <source media='(min-width: 768px)' srcSet={logoLarge} />
         <img src={logo} alt='Marko Nikolajevic Logo' />
       </picture>
     </div>

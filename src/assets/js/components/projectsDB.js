2f8e29c87058de731bb202817aab75dd553433c1
Marko Nikolajevic <mrknikolajevic@gmail.com>
2019-05-12 16:46:29 +0200

    Add images for projects

--- src/assets/js/components/projectsDB.js
+++ src/assets/js/components/projectsDB.js
@@ -1,24 +1,32 @@
+import billWebp from '../../img/billlee.webp'
+import billPng from '../../img/billlee.png'
+import camlabWebp from '../../img/camlab.webp'
+import camlabPng from '../../img/camlab.png'
+import memoryWebp from '../../img/memory-game.webp'
+import memoryPng from '../../img/memory-game.png'
+
+
 const projectsDB = [
   {
     id: 'billLee',
-    webPImage: '#',
-    image: '../../img/billlee.png',
+    webPImage: billWebp,
+    image: billPng,
     title: 'Bill Lee',
     description: 'Sito personale del grande artista e musicista Bill Lee Curtis.',
     link: 'https://www.billleecurtis.com/'
   },
   {
     id: 'camlab',
-    webPImage: '#',
-    image: '#',
+    webPImage: camlabWebp,
+    image: camlabPng,
     title: 'CamLab Trieste',
     description: 'Gruppo professionale di wellness e cura della persona situato a Trieste.',
     link: 'https://www.camlabtrieste.com/'
   },
   {
     id: 'memoryGame',
-    webPImage: '#',
-    image: '#',
+    webPImage: memoryWebp,
+    image: memoryPng,
     title: 'Memory Game',
     description: 'Browser game creato per il programma Front End NanoDegree su Udacity.',
     link: 'https://www.markonikolajevic.com/memory-game/'

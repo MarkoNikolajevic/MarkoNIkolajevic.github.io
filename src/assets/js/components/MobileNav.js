7dccb47d489bfe2511afdc6ed12d9564ac1471b0
Marko Nikolajevic <mrknikolajevic@gmail.com>
2019-05-13 13:50:30 +0200

    Indent NavLink

--- src/assets/js/components/MobileNav.js
+++ src/assets/js/components/MobileNav.js
@@ -1,21 +1,19 @@
 import React from 'react'
 import NavLink from './NavLink'
 import FooterLinks from './FooterLinks'
-import logo from '../../img/monogramma.svg'
 
 function MobileNav() {
   return (
     <nav className='nav-mobile' role='navigation'>
-    <NavLink
-      link={{url: '#about', text: 'Su di me'}}
-    />
-    <NavLink
-      link={{url: '#portfolio', text: 'Portfolio'}}
-    />
-    <NavLink
-      link={{url: '#contact', text: 'Contatti'}}
-    />
-    <img src={logo} width='100px' alt='Marko Nikolajević Logo'/>
+      <NavLink
+        link={{url: '#about', text: 'Su di me'}}
+      />
+      <NavLink
+        link={{url: '#portfolio', text: 'Portfolio'}}
+      />
+      <NavLink
+        link={{url: '#contact', text: 'Contatti'}}
+      />
       <div className='wrapper'>
         <FooterLinks
           footerLink={{url: 'https://twitter.com/markez1317', icon: 'fab fa-twitter'}}

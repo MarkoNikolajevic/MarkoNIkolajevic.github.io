33150b9e59abb6097c0a5aecf5c7b6561c5bd6e1
Marko Nikolajevic <mrknikolajevic@gmail.com>
2019-05-13 13:51:16 +0200

    Code mobile nav animation and menu animation

--- src/assets/js/components/HamburgerMenu.js
+++ src/assets/js/components/HamburgerMenu.js
@@ -12,17 +12,37 @@ class HamburgerMenu extends React.Component {
     this.setState(state => ({
       isActive: !state.isActive
     }));
+    const hamburger = document.querySelector('.hamburger-menu');
+    const navMobile = document.querySelector('.nav-mobile');
+
+    hamburger.classList.toggle('animated');
+    navMobile.classList.toggle('show-menu-mobile');
+    closeNav();
   }
 
   render() {
     return (
-      <div className='hamburger-menu' onClick={this.handleClick}>
+      <div className='hamburger-menu' onClick={this.handleClick} aria-label='Menu' aria-controls='navigation'>
         <div className='bar'></div>
         <div className='bar'></div>
         <div className='bar'></div>
       </div>
     )
   }
+}
+
+// Close nav function
+function closeNav() {
+  let links = document.querySelectorAll('.nav-mobile-link');
+  links = Array.from(links);
+  links.forEach(function(elem) {
+    elem.addEventListener('click', function() {
+      const hamburger = document.querySelector('.hamburger-menu');
+      const nav = document.querySelector('.nav-mobile');
+      nav.classList.remove('show-menu-mobile');
+      hamburger.classList.remove('animated');
+    });
+  });
 }
 
 export default HamburgerMenu

af908f3e860fa44eabae84474898c7854f8e03be
Marko Nikolajevic <mrknikolajevic@gmail.com>
2020-06-01 19:19:35 +0200

    set up routing

--- src/App.js
+++ src/App.js
@@ -1,22 +1,31 @@
 import React from 'react';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 
 import './assets/style/App.sass';
 
-import Header from './assets/js/components/Header';
-import About from './assets/js/components/About';
-import Portfolio from './assets/js/components/Portfolio';
-import Form from './assets/js/components/Form';
+import ScrollToTop from './assets/js/components/ScrollToTop';
+import Navbar from './assets/js/components/Navbar';
+import Home from './assets/js/pages/Home';
+import PortfolioPage from './assets/js/pages/PortfolioPage';
+import ContactPage from './assets/js/pages/ContactPage';
 import Footer from './assets/js/components/Footer';
 
 function App() {
   return (
-    <div className='App'>
-      <Header />
-      <About />
-      <Portfolio />
-      <Form />
+    <Router>
+      <ScrollToTop />
+      <Navbar />
+      <Switch>
+        <Route exact path='/' component={Home} />
+      </Switch>
+      <Switch>
+        <Route path='/portfolio' component={PortfolioPage} />
+      </Switch>
+      <Switch>
+        <Route path='/contatti' component={ContactPage} />
+      </Switch>
       <Footer />
-    </div>
+    </Router>
   );
 }
 
